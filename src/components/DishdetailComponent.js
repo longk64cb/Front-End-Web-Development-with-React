37171ea122ef64746f8cbae0ccbbb2592239a804
Tran Dinh Long <longa5k50@gmail.com>
2021-04-05 21:38:30 +0700

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -18,7 +18,7 @@ import { baseUrl } from '../shared/baseUrl';
         );
       }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         const comment = comments.map((comment) => {
             return (
                 <li>
@@ -40,7 +40,7 @@ import { baseUrl } from '../shared/baseUrl';
                 <ul className="list-unstyled">
                     {comment}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         )
     }
@@ -83,7 +83,7 @@ import { baseUrl } from '../shared/baseUrl';
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <RenderComments comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id}
                         />
                     </div>
@@ -118,7 +118,7 @@ import { baseUrl } from '../shared/baseUrl';
 
         handleSubmit(values) {
             this.toggleCommentModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+            this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         }
 
         render() {
